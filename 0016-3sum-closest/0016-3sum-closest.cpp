78c8281df0515f5ec4300c9a49c84e74c1c6a879
Ahmad ali <i250817@isb.nu.edu.pk>
2026-08-07 02:18:12 +0500

    Time: 8 ms (74.8%), Space: 13.9 MB (99.43%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,4 +1,4 @@
-
+/*
 
 /////////////////////////////
 
@@ -32,4 +32,72 @@ int threeSumClosest(vector<int>& nums, int target) {
 
     return sum;
 }
+};
+
+
+It o(n^3) which takes 835 ms to run along with takes 14.14 mb which is a lot 
+so i need to do it with double pointer 
+
+*/
+
+
+/////////////////////////////
+
+// max i shall take is O(n^2)
+
+////////////////////////////
+
+class Solution {
+public:
+int threeSumClosest(vector<int>& nums, int target) {
+
+    int sum = 0;
+    int diff;
+    int maxDiff = INT_MAX;
+
+
+    sort(nums.begin(), nums.end());// as two pointer aka binary search work for only sorted array 
+
+
+    for(int i = 0 ; i < nums.size() ; i++){
+
+
+        int j = i + 1;
+        int k = nums.size() - 1;
+
+
+        while(j < k){  //it might cross over k 
+
+
+            int current = nums[i] + nums[j] + nums[k];
+
+            
+            diff = current - target;
+
+
+            if(diff > 0){
+                k--;
+            }
+            else if(diff < 0){
+                j++;
+            }
+            else{
+                return current;
+            }
+
+
+            if(abs(diff) < maxDiff){
+                maxDiff = abs(diff);
+                sum = current;
+            }
+
+
+
+        }
+    }
+
+
+    return sum;
+}
+
 };
