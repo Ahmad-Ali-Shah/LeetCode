eba21f6d947887a2bb6cd5e6bc5a68cbd6101ab1
Ahmad ali <i250817@isb.nu.edu.pk>
2026-08-07 02:08:14 +0500

    Time: 859 ms (5.01%), Space: 14 MB (85.65%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,33 @@
+/////////////////////////////
+
+// max i shall take is O(n^2)
+
+////////////////////////////
+
+class Solution {
+public:
+int threeSumClosest(vector<int>& nums, int target) {
+
+    int sum = 0;
+    unsigned int diff = INT_MAX;
+
+    for(int i = 0; i < nums.size(); i++){
+
+        for(int j = i + 1; j < nums.size(); j++){
+
+            for(int k = j + 1; k < nums.size(); k++){
+
+                int current = nums[i] + nums[j] + nums[k];
+
+                if(abs(current - target) < diff){
+                    diff = abs(current - target);
+                    sum = current;
+                }
+
+            }
+        }
+    }
+
+    return sum;
+}
+};
