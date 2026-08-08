866794f30bb1b3f838fe5601e10925da341dd7e7
Ahmad ali <i250817@isb.nu.edu.pk>
2026-08-08 16:34:06 +0500

    Time: 0 ms (100%), Space: 22.5 MB (98.31%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
+int removeDuplicates(vector<int>& nums) {
+/*
+int sizeOf = nums.size();
 
-        int sizeOf = nums.size();
+    for(int i = 0; i < sizeOf; i++){
+        for(int j = i + 1; j < sizeOf; j++){
+            if(nums[i] == nums[j]){
 
-        for(int i = 0; i < sizeOf; i++){
-            for(int j = i + 1; j < sizeOf; j++){
-                if(nums[i] == nums[j]){
-
-                    for(int k = j; k < sizeOf - 1; k++){
-                        swap(nums[k], nums[k + 1]);
-                    }
-
-                    sizeOf--;
-                    j--; //so it starts comparing from same 
+                for(int k = j; k < sizeOf - 1; k++){
+                    swap(nums[k], nums[k + 1]);
                 }
+
+                sizeOf--;
+                j--; //so it starts comparing from same 
             }
         }
-
-        return sizeOf;
     }
+
+    return sizeOf;
+}
+
+
+ its o(n^2) which is too slow so i need to fix it 
+*/
+int found = 0;
+
+for(int num = 1; num < nums.size(); num++){
+    if(nums[num] != nums[found]){
+        found++;
+        nums[found] = nums[num]; //move it forward and forward
+    }
+}
+
+return found + 1;
+}
+
 };
