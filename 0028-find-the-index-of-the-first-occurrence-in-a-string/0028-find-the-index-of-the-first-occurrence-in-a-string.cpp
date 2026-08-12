1bcbdb978f9c730992bc598f5edb94dc8f754d17
Ahmad ali <i250817@isb.nu.edu.pk>
2026-08-13 01:23:21 +0500

    Time: 0 ms (100%), Space: 9.1 MB (45.06%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,3 +1,5 @@
+/*
+lesser optimized code
 class Solution {
 public:
     int strStr(string haystack, string needle) {
@@ -25,6 +27,50 @@ public:
                 break;
             }
             
+            index++;
+        }
+
+        if(!isFound){
+            return -1;
+        }
+        else{
+            return index;
+        }
+    }
+};
+
+
+*/
+
+
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+
+        int index = 0;
+        int strLength = needle.length(); //.length for getting length of string
+        bool isFound = false;
+
+        if(strLength > haystack.length()){
+            return -1;
+        }
+
+        while(index <=( haystack.length() - needle.length()))
+        {
+            int i = 0;
+
+            for(i = 0; i < needle.length(); i++){
+                if(haystack[index + i] != needle[i]){
+                    break;
+                }
+            }
+
+            if(i == needle.length()){
+                isFound = true;
+                break;
+            }
+
             index++;
         }
 
