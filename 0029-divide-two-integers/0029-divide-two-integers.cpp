4cf17ee490542753f324a6f24d5e18433f23be92
Ahmad ali <i250817@isb.nu.edu.pk>
2026-08-13 02:39:38 +0500

    Time: 2709 ms (5%), Space: 8.7 MB (40.23%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,13 +1,24 @@
-class Solution {
+class Solution{
 public:
     int divide(int dividend, int divisor) {
         bool isNeg = false;
 
         if(dividend < 0 && divisor < 0){
             isNeg = false;
+            
+            if(divisor == -1){
+                if(dividend == -2147483648LL){
+                    return 2147483647;
+                }
+                return -dividend;
+            }
         }
         else if(dividend < 0 || divisor < 0){
             isNeg = true;
+
+            if(divisor == 1){
+                return dividend;
+            }
         }
 
         unsigned long long div = abs((long long)dividend);
@@ -16,17 +27,8 @@ public:
         long long res = 0;
 
         while(div >= dio){
-
-            unsigned long long temp = dio;
-            long long count = 1;
-
-            while(div >= temp + temp){
-                temp = temp + temp;
-                count = count + count;
-            }
-
-            div = div - temp;
-            res = res + count;
+            div -= dio;
+            res++;
         }
 
         if(isNeg){
