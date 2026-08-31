d92c995171e5ba110055f8d7a6527b7112475922
Ahmad ali <i250817@isb.nu.edu.pk>
2026-09-01 00:02:32 +0500

    Time: 0 ms (100%), Space: 100.5 MB (33.74%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -5,7 +5,7 @@ public:
         for(int i = 0; i < nums.size() - 1; i++){
 
             if(nums[i] == nums[i + 1]){
-                continue;
+                continue; //skip the  same ones 
             }
 
             bool isMonotonicsIncreasing = (nums[i] < nums[i + 1]);
